42fef558ab99a1a1e77cd9863f29df13df162fd8
Ben Sterrett <raynjamin@gmail.com>
2017-09-29 11:56:07 -0400

    Initial Lot Bot

---
+++ src/main/java/com/example/lotbot/Space.java
@@ -0,0 +1,6 @@
+package com.example.lotbot;
+
+
+// TODO: Write this.
+public class Space {
+}
---
+++ src/main/java/com/example/lotbot/Transaction.java
@@ -0,0 +1,9 @@
+package com.example.lotbot;
+
+import java.time.LocalDateTime;
+
+// TODO: Finish fields, introduce getters/setters
+public class Transaction {
+    private LocalDateTime checkedInDate;
+    private LocalDateTime checkedOutDate;
+}
